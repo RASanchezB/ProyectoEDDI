de877811eda9b53d7ae7b2440f10be59648d9db7
RASanchezB <rasanchez@unitec.edu>
2018-04-08 00:04:39 -0600

    Una nueva clase

--- src/StackLab.java
+++ src/StackLab.java
@@ -1,15 +1,24 @@
+
+import java.util.ArrayList;
+
 public class StackLab {
-    private char caracter;
+    private ArrayList<NodoLab> lista = new ArrayList();
     private int PosX;
     private int PosY;
 
     public StackLab() {
     }
-    public StackLab(char caracter, int PosX, int PosY) {
-        this.caracter = caracter;
+    public StackLab(int PosX, int PosY) {
         this.PosX = PosX;
         this.PosY = PosY;
     }
-    
-            
+    public boolean push(NodoLab nod){
+        if(lista.get(0)==null){
+            lista.add(new NodoLab(PosX,PosY));
+        } else{
+            NodoLab data = (new NodoLab(PosX,PosY));
+            lista.add(0, data);
+        }
+        return true;
+    }
 }
